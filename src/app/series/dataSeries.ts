68216c0e573ad34e57abcb6b627760a4da905e36
Victoria González <vs.gonzalezc1@uniandes.edu.co>
2025-04-25 07:08:34 -0500

    Imagenes

--- src/app/series/dataSeries.ts
+++ src/app/series/dataSeries.ts
@@ -6,17 +6,17 @@ export const dataSeries: Serie[] = [
     'Breaking Bad',
     'AMC',
     5,
-    'A high school chemistry teacher turned methamphetamine producer partners with a former student.',
-    'https://upload.wikimedia.org/wikipedia/en/6/61/Breaking_Bad_title_card.png',
-    'https://www.amc.com/shows/breaking-bad'
+    "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHGZV4jVq5iq7S7FeUDNTjSuxwuvCI8n0Jfg&s",
+    "https://www.amc.com/shows/breaking-bad"
   ),
   new Serie(
     2,
     'Orange Is the New Black',
     'Netflix',
     6,
-    'Convicted of a decade old crime of transporting drug money, Piper Chapman is sentenced to a year and a half behind bars.',
-    'https://upload.wikimedia.org/wikipedia/en/0/0b/Orange_Is_the_New_Black_season_1_poster.jpg',
+    "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary",
+    "https://akns-images.eonline.com/eol_images/Entire_Site/201869/rs_634x939-180709061638-634-oitnb-s6.ch.070918.jpg?fit=around%7C634:939&output-quality=90&crop=634:939;center,top",
     'https://www.netflix.com/title/70242311'
   ),
   new Serie(
@@ -24,35 +24,35 @@ export const dataSeries: Serie[] = [
     'The Big Bang Theory',
     'CBS',
     12,
-    'A woman moves into an apartment across the hall from two brilliant but socially awkward physicists.',
-    'https://upload.wikimedia.org/wikipedia/en/6/6f/The_Big_Bang_Theory_Season_6.jpg',
-    'https://www.cbs.com/shows/big_bang_theory/'
-  ),
-  new Serie(
-    4,
-    'Sherlock',
-    'BBC',
-    4,
-    'A modern update finds the famous sleuth and his doctor partner solving crime in 21st century London.',
-    'https://upload.wikimedia.org/wikipedia/en/b/b9/Sherlock_titlecard.jpg',
-    'https://www.bbc.co.uk/programmes/b018ttws'
+    "Leonard and Sheldon are brilliant physicists—geniuses in the laboratory but socially challenged everywhere else. Enter beautiful, street-smart neighbor Penny, who aims to teach them a thing or two about life. Despite their on-again, off-again relationship in the past, Leonard and Penny have finally gotten married. Even Sheldon has found a female companion, entering into a relationship agreement with neurobiologist Amy Farrah Fowler, and he recently took their relationship to the next level by marrying her after a long courtship. In their free time, Leonard and Sheldon enjoy fantasy role-playing games with their ever-expanding universe of friends, including fellow scientists Koothrappali, Wolowitz, and Wolowitz’s adorable microbiologist wife, Bernadette, who is adjusting to life with their two children.",
+    "https://m.media-amazon.com/images/S/pv-target-images/4b63ef0284ce2942556d96bd39cef16f93384e974dabe61ff5e6b0ae1d794194.jpg",
+    'https://www.cbs.com/shows/big_bang_theory/about'
   ),
   new Serie(
     5,
+    'Sherlock',
+    'BBC',
+    4,
+    "Sherlock depicts consulting detective Sherlock Holmes (Benedict Cumberbatch) solving various mysteries in modern-day London. Holmes is assisted by his flatmate and friend, Dr John Watson (Martin Freeman), who has returned from military service in Afghanistan with the Royal Army Medical Corps",
+    "https://m.media-amazon.com/images/M/MV5BNTQzNGZjNDEtOTMwYi00MzFjLWE2ZTYtYzYxYzMwMjZkZDc5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    'https://www.bbc.co.uk/programmes/b018ttws'
+  ),
+  new Serie(
+    4,
     'A Very English Scandal',
     'BBC',
-    1,
-    'British Liberal Party leader Jeremy Thorpe is accused of conspiracy to murder his former lover in 1979.',
-    'https://upload.wikimedia.org/wikipedia/en/1/11/A_Very_English_Scandal_titlecard.jpg',
+    2,
+    "A Very English Scandal is a fact-based three-part British television comedy-drama miniseries based on John Preston's book of the same name.",
+    "https://i.blogs.es/989bca/a-very-english-scandal-critica-2018-espinof/1366_521.jpg",
     'https://www.bbc.co.uk/programmes/p065smy4'
   ),
   new Serie(
-    6,
-    'Stranger Things',
-    'Netflix',
-    4,
-    'When a young boy vanishes, a small town uncovers a mystery involving secret experiments, supernatural forces, and a strange girl.',
-    'https://upload.wikimedia.org/wikipedia/en/f/f7/Stranger_Things_season_4.jpg',
-    'https://www.netflix.com/title/80057281'
+    3,
+    "Game of Thrones",
+    "HBO",
+    7,
+    "American fantasy drama",
+    "https://m.media-amazon.com/images/M/MV5BMTNhMDJmNmYtNDQ5OS00ODdlLWE0ZDAtZTgyYTIwNDY3OTU3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+    "https://www.hbo.com/game-of-thrones"
   )
 ];
